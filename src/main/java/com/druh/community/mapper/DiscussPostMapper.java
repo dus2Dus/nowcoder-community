8e447f671c66542cb6d10c3454729601222027d3
DRUH <rushdjy@gmail.com>
2023-01-15 20:59:11 +0800

    配置logback，添加了logback-spring.xml

--- src/main/java/com/druh/community/mapper/DiscussPostMapper.java
+++ src/main/java/com/druh/community/mapper/DiscussPostMapper.java
@@ -22,6 +22,7 @@ public interface DiscussPostMapper {
     /**
      * 查询userId来查询这个用户共发了多少条帖子
      * @param userId
+     * @Param注解用于给参数取别名，当这个方法中只有一个参数时并且这个参数在mapper.xml文件中被使用在<if>标签中时，就必须要使用@Param注解来给这个参数取个别名
      * @return
      */
     int selectDiscussPostRows(@Param("userId") int userId);
