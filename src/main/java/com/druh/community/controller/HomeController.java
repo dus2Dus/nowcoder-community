c042352a021cba8d44e23c872ac9a3514de186af
DRUH <rushdjy@gmail.com>
2023-05-04 15:15:33 +0800

    完成统一处理异常、统一记录日志功能

--- src/main/java/com/druh/community/controller/HomeController.java
+++ src/main/java/com/druh/community/controller/HomeController.java
@@ -8,6 +8,7 @@ import com.druh.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -48,5 +49,14 @@ public class HomeController {
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+    /**
+     * 返回500错误页面
+     * @return
+     */
+    @GetMapping("/error")
+    public String getErrorPage() {
+        return "/error/500";
     }
 }
